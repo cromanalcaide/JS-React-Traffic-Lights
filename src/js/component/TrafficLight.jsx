2a0d50668666136ffeb1f298867dcd919aca5f4e
cromanalcaide <cromanalcaide@gmail.com>
2022-11-29 16:32:08 +0000

    adding the set functions

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -2,17 +2,22 @@ import React from "react";
 
 const TrafficLight = () => {
 
-// const [encendido, setEncendido] = useState (false);
-// setEncendido = () => {
-//     console.log("This is apagado right now")
-// }
+const [redOn, setRedOn] = useState (false);
+// const [orangeOn, setOrangeOn] = useState (false);
+// const [greenOn, setGreenOn] = useState (false);
+
+ const switchRedOn = event => {
+     setRedOn(redOn => !redOn);
+    
+   };
 
 console.log("hola pringao");
 
 return (
 <div className="itemAligner">
     <div className="trafficLightFrame">
-        <div className="redOff" onClick={() => encendido()}></div>
+        <div className={redOn ? 'redOn' : 'redOff'} onClick={() => switchRedOn()}></div>
+        {/* <button className={isActive ? 'bg-salmon' : ''} onClick={handleClick}></button> */}
         <div className="orangeOff" onClick={() => encendido()}></div>
         <div className="greenOff" onClick={() => encendido()}></div>
     </div>
